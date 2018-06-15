b2dc33ed1683877481eb40de011a11e5daddec21
Adam Powers <apowers@ato.ms>
2018-06-14 21:30:58 -0700

    add features, links

---
+++ test/parseAndroidSafetyNetAttestationDataTest.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const parser = require("../lib/parser");
+var assert = require("chai").assert;
+const h = require("fido2-helpers");
+var {
+    coerceToBase64,
+    abEqual,
+    printHex,
+} = require("../lib/utils");
+
+describe("parseAttestationObject (tpm)", function() {
+    it("parser is object", function() {
+        assert.isObject(parser);
+    });
+
+    var ret;
+    it("can parse", function() {
+        ret = parser.parseAttestationObject(h.lib.makeCredentialAttestationSafetyNetResponse.response.attestationObject);
+        // console.log("ret", ret);
+    });
+});
