50e9cf69859f7ee53fc8596051275ed4e6c1fc7f
Cody Nicewanner <cody@dfns.co>
2022-12-30 10:50:50 -0800

    Making the asn1 tests use static values. Moving from Buffer to Javascript native types for padding the array in derToRaw.

---
+++ test/fixtures/ecdsaPublicKey.js
@@ -0,0 +1,7 @@
+// privateKey = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgu9nv4cQyNEFUkAfb\n3KwStEnfml2iyzgFGVSnIBHrDjuhRANCAAQkFAXLiJbHHbIw7r7xnGN36pWb54FV\na+lkH5NN27NE2ZKytnq6jF04++6rdDgxJ4qkPX7QrrfQuuCFhujSZ+KH\n-----END PRIVATE KEY-----\n"
+const ecdsaPublicKey = {
+	examplePem:
+		"-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEJBQFy4iWxx2yMO6+8Zxjd+qVm+eB\nVWvpZB+TTduzRNmSsrZ6uoxdOPvuq3Q4MSeKpD1+0K630LrghYbo0mfihw==\n-----END PUBLIC KEY-----\n",
+};
+
+export { ecdsaPublicKey };
